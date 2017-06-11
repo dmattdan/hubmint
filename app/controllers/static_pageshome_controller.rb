f4b364ca24d45b05d523db2325642012bd134c2c
matt <dmattdanielson@icloud.com>
2017-06-11 00:19:02 -0500

    Add a Static Pages controller

--- app/controllers/static_pageshome_controller.rb
+++
@@ -1,4 +0,0 @@
-class StaticPageshomeController < ApplicationController
-  def help
-  end
-end
--- test/controllers/static_pageshome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StaticPageshomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get help" do
-    get static_pageshome_help_url
-    assert_response :success
-  end
-
-end
